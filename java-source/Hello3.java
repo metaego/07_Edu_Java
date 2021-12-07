de5d21b091058f600b97bf9e85ef21e014d3ad6f
kimself <ksy7678@gmail.com>
2021-12-07 19:38:28 +0900

    define field and method

---
+++ java-source/Hello3.java
@@ -0,0 +1,24 @@
+public class Hello3 {
+
+    // 필드 정의
+    int age;
+
+    // int -> 키워드
+    // age -> 식별자(변수)
+
+    int myAge = 10;
+
+    // 메서드(함수)
+
+    // 두개의 숫자값을 입력받아서 높은 값을 구하는 메서드
+    // 매개변수(필수는 아님)
+    // 리턴타입(필수는 아님)
+    int maxNumber(int x, int y){
+        return 0;
+    }
+
+    // 리턴타입이 없는 경우 void를 사용
+    void maxNumber(){
+    }
+
+}
