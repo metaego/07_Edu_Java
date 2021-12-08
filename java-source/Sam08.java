874c350b249f5f243ffa554d5babc6f6836c32be
kimself <ksy7678@gmail.com>
2021-12-08 15:01:59 +0900

    learned variable

---
+++ java-source/Sam08.java
@@ -0,0 +1,20 @@
+public class Sam08 {
+
+    public static void main(String[] args) {
+
+        // 상수 -> 기본형(불린형, 정수, 실수, 문자형, 문자열)
+        int i1 = 10;
+        long l1 = 20L;
+        double d1 = 3.1D;
+        double d2 = 3.1E2;
+        double d3 = 0x1.1P2;
+
+        System.out.println(i1);
+        System.out.println(l1);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3 );
+
+    } 
+    
+}
