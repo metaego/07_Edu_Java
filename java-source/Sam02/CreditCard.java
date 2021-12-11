2001bcaa03ce512709fa4fc0f70a99808549915e
kimself <ksy7678@gmail.com>
2021-12-12 08:20:57 +0900

    add getter and setter

--- java-source/Sam02/CreditCard.java
+++ java-source/Sam02/CreditCard.java
@@ -4,5 +4,34 @@ public class CreditCard {
 
     public long cardNumber;
     public String cardOwner;
-    
+
+    public long balance;
+    public long point; // this.point의 point
+ 
+    public void use(long amount) {
+        balance += amount;
+    }
+
+    public void payBill(long amount) {
+        balance -= amount;
+        addPoint(amount / 1000);
+    }
+    private void addPoint(long point) {
+        this.point += point;
+    }
+
+    // setter(값을 설정함)
+    public void setCardNumber(long cardNumber) {
+        // this.cardNumber = cardNumber;
+        if (cardNumber < 1000_0000_0000_0000L) {
+            System.err.println("카드번호에문제가 있다");
+        } else {
+            this.cardNumber = cardNumber;
+        }
+    }
+
+    // getter(값을 가져옴)
+    public long getCardNumber() {
+        return cardNumber;
+    }
 }
--- java-source/Sam02/CreditCardTest.java
+++ java-source/Sam02/CreditCardTest.java
@@ -5,12 +5,28 @@ public class CreditCardTest {
     public static void main(String[] args) {
 
         CreditCard myCard = new CreditCard();
-        myCard.cardNumber = 1234_5678_1234_1234L;
+        // myCard.cardNumber = 1234_5678_1234_1234L;
+        myCard.setCardNumber(1234_5678_1234_1234L);
         myCard.cardOwner = "홍길동";
 
-        System.out.println(myCard.cardNumber);
+        System.out.println("=======================");
+        // System.out.println(myCard.cardNumber);
+        System.out.println(myCard.getCardNumber());
         System.out.println(myCard.cardOwner);
+        System.out.println(myCard.balance);
+        System.out.println(myCard.point);
 
+        myCard.use(8000);
+        myCard.use(12000);
+        myCard.use(21000);
+        myCard.payBill(30000);
+
+        System.out.println("=======================");
+        // System.out.println(myCard.cardNumber);
+        System.out.println(myCard.getCardNumber());
+        System.out.println(myCard.cardOwner);
+        System.out.println(myCard.balance);
+        System.out.println(myCard.point);
 
     }
     
